e102cd6966f5a5faa22921e61938f6aa36b494b6
dana <danananananas@gmail.com>
2020-12-04 00:43:34 +0100

    oopsie

--- day01/solution.py
+++ day01/solution.py
@@ -1,7 +1,7 @@
 from helper import read_input
 
 
-def get_expenses_report (filename='input.txt'):
+def get_expenses_report(filename='input.txt'):
     report = read_input(filename)
     report = [int(expense) for expense in report]
     return report
