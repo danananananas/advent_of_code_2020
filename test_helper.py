2dabd7d466fda2efc66fa8e82d8fb31864fdffb6
dana <danananananas@gmail.com>
2020-12-03 22:50:52 +0100

    test open() was called with the filename

--- test_helper.py
+++ test_helper.py
@@ -7,13 +7,13 @@ class TestFirst(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data='1\n2\n3')
     def test_read_input(self, m_open):
         result = read_input('/dummy/filename')
-        m_open.assert_called_once()
+        m_open.assert_called_once_with('/dummy/filename')
         self.assertListEqual(['1', '2', '3'], result)
 
     @patch('builtins.open', new_callable=mock_open, read_data='1 \n 2 \n 3\n')
     def test_read_input_strips_white_lines(self, m_open):
         result = read_input('/dummy/filename')
-        m_open.assert_called_once()
+        m_open.assert_called_once_with('/dummy/filename')
         self.assertListEqual(['1', '2', '3'], result)
 
 
