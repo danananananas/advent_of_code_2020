b30f9aeb5a66004604c830eb7165797aea39479d
dana <danananananas@gmail.com>
2020-12-04 00:43:08 +0100

    no need to iterate for third expanse, just check if it's in the report; but, iterate for second expense instead of taking always the next one

--- day01/solution.py
+++ day01/solution.py
@@ -14,20 +14,27 @@ def find_pair_with_sum_2020(report):
                 return first_expense, second_expense
 
 
+def find_triple_with_sum_2020(report):
+    for i, first_expense in enumerate(report[:-1]):
+        for second_expense in report[i + 1:]:
+            if (first_and_second := first_expense + second_expense) < 2020:
+                remaining = 2020 - first_and_second
+                if remaining in report:
+                    return first_expense, second_expense, remaining
+
+
 def first_solution():
     report = get_expenses_report()
     first_expense, second_expense = find_pair_with_sum_2020(report)
     return first_expense * second_expense
 
 
-def find_triple_with_sum_2020(report):
-    for i, first_expense in enumerate(report[:-2]):
-        second_expense = report[i + 1]
-        if (first_and_second := first_expense + second_expense) < 2020:
-            for third_expense in report[i + 2:]:
-                if first_and_second + third_expense == 2020:
-                    return first_expense, second_expense, third_expense
+def second_solution():
+    report = get_expenses_report()
+    first_expense, second_expense, third_expense = find_triple_with_sum_2020(report)
+    return first_expense * second_expense * third_expense
 
 
 if __name__ == '__main__':
     print(first_solution())
+    print(second_solution())
