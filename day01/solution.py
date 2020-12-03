33eb9aa2fda9c9c38c3df053acc7cfe677459daa
dana <danananananas@gmail.com>
2020-12-03 23:03:09 +0100

    get expenses report to call helper.read_input and return list of ints

--- day01/solution.py
+++ day01/solution.py
@@ -1,2 +1,7 @@
-def get_expenses_report(filename):
-    pass
+from helper import read_input
+
+
+def get_expenses_report(filename='input.txt'):
+    report = read_input(filename)
+    report = [int(expense) for expense in report]
+    return report
--- day01/test_solution.py
+++ day01/test_solution.py
@@ -1,14 +1,20 @@
 import unittest
 from unittest.mock import patch, mock_open
-from solution import get_expenses_report
+from day01.solution import get_expenses_report
 
 
 class TestSolution(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data='1\n2\n3')
-    def test_read_input(self, m_open):
+    def test_read_input_mock_open(self, m_open):
         result = get_expenses_report('/dummy/filename')
         m_open.assert_called_once_with('/dummy/filename')
         self.assertListEqual([1, 2, 3], result)
+
+    @patch('builtins.open', new_callable=mock_open, read_data='1721\n979\n366\n299\n675\n1456')
+    def test_read_input_example(self, m_open):
+        result = get_expenses_report('/dummy/filename')
+        m_open.assert_called_once_with('/dummy/filename')
+        self.assertListEqual([1721, 979, 366, 299, 675, 1456], result)
 
 
 if __name__ == '__main__':
