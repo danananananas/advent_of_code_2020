d9ab2fbaddfb60cbd80001b001c845acfc827e43
dana <danananananas@gmail.com>
2020-12-04 14:27:12 +0100

    rename functions and variables to make sense for the The Official Toboggan Corporate Policy, too

--- day02/solution.py
+++ day02/solution.py
@@ -6,16 +6,16 @@ def read_pwd_line(line):
     """Read a line of format '<num>-<num> <char>: <string>' and returns a tuple
        (int(num), int(num), char, string)"""
     result = re.split(r' |-|: ', line)
-    min_limit, max_limit, *result = result
-    return int(min_limit), int(max_limit), *result
+    first_num, second_num, *result = result
+    return int(first_num), int(second_num), *result
 
 
-def check_pwd_policy(min_limit, max_limit, char, pwd):
+def check_sled_rental_pwd_policy(min_limit, max_limit, char, pwd):
     return min_limit <= pwd.count(char) <= max_limit
 
 
 def count_valid_pwds(lines):
-    valid_pwds = [check_pwd_policy(*read_pwd_line(line)) for line in lines]
+    valid_pwds = [check_sled_rental_pwd_policy(*read_pwd_line(line)) for line in lines]
     return sum(valid_pwds)
 
 
--- day02/test_solution.py
+++ day02/test_solution.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from day02.solution import read_pwd_line, check_pwd_policy, count_valid_pwds
+from day02.solution import read_pwd_line, check_sled_rental_pwd_policy, count_valid_pwds
 
 
 class TestSolution(TestCase):
@@ -20,15 +20,15 @@ class TestSolution(TestCase):
 
     def test_check_pwd_policy_example_1(self):
         test_instance = (1, 3, 'a', 'abcde')
-        self.assertTrue(check_pwd_policy(*test_instance))
+        self.assertTrue(check_sled_rental_pwd_policy(*test_instance))
 
     def test_check_pwd_policy_example_2(self):
         test_instance = (1, 3, 'b', 'cdefg')
-        self.assertFalse(check_pwd_policy(*test_instance))
+        self.assertFalse(check_sled_rental_pwd_policy(*test_instance))
 
     def test_check_pwd_policy_example_3(self):
         test_instance = (2, 9, 'c', 'ccccccccc')
-        self.assertTrue(check_pwd_policy(*test_instance))
+        self.assertTrue(check_sled_rental_pwd_policy(*test_instance))
 
     def test_find_valid_pwds_example(self):
         test_instance = [
