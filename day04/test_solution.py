5216c9a1a58d48160cf82bbae8ad93b49a746c62
dana <danananananas@gmail.com>
2020-12-05 14:00:26 +0100

    parse_line(line) should also map the line to a dictionary

--- day04/test_solution.py
+++ day04/test_solution.py
@@ -10,23 +10,18 @@ class TestSolution(TestCase):
         m_open.assert_called_once_with('/dummy/filename')
         self.assertListEqual(['1\r\n2', '3'], result)
 
-    def test_parse_lines(self):
-        test_line = '1\r\n2'
-        expected_result = ['1', '2']
-        self.assertListEqual(expected_result, parse_line(test_line))
-
     def test_parse_lines_example_without_newline(self):
         test_line = 'byr:1991 eyr:2022 hcl:#341e13 iyr:2016 pid:729933757 hgt:167cm ecl:gry'
-        expected_result = [
-            'byr:1991', 'eyr:2022', 'hcl:#341e13', 'iyr:2016',
-            'pid:729933757', 'hgt:167cm', 'ecl:gry'
-        ]
-        self.assertListEqual(expected_result, parse_line(test_line))
+        expected_result = {
+            'byr': '1991', 'eyr': '2022', 'hcl': '#341e13', 'iyr': '2016',
+            'pid': '729933757', 'hgt': '167cm', 'ecl': 'gry'
+        }
+        self.assertDictEqual(expected_result, parse_line(test_line))
 
     def test_parse_lines_example_with_newline(self):
         test_line = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\nbyr:1937 iyr:2017 cid:147 hgt:183cm'
-        expected_result = [
-            'ecl:gry', 'pid:860033327', 'eyr:2020', 'hcl:#fffffd',
-            'byr:1937', 'iyr:2017', 'cid:147', 'hgt:183cm'
-        ]
-        self.assertListEqual(expected_result, parse_line(test_line))
+        expected_result = {
+            'ecl': 'gry', 'pid': '860033327', 'eyr': '2020', 'hcl': '#fffffd',
+            'byr': '1937', 'iyr': '2017', 'cid': '147', 'hgt': '183cm'
+        }
+        self.assertDictEqual(expected_result, parse_line(test_line))
