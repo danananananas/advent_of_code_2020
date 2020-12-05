af7a91da0859017a49179c4375797b376136cce0
dana <danananananas@gmail.com>
2020-12-05 13:48:30 +0100

    parse_line(line) should split the input further on either whitespace or newline

--- day04/solution.py
+++ day04/solution.py
@@ -1,2 +1,7 @@
 def read_input(filename):
+    with open(filename) as f:
+        return f.read().split('\r\n\r\n')
+
+
+def parse_lines(lines):
     pass
--- day04/test_solution.py
+++ day04/test_solution.py
@@ -1,11 +1,16 @@
 from unittest import TestCase
 from unittest.mock import patch, mock_open
-from day04.solution import read_input
+from day04.solution import read_input, parse_lines
 
 
 class TestSolution(TestCase):
-    @patch('builtins.open', new_callable=mock_open, read_data='1\n2\n\n3')
+    @patch('builtins.open', new_callable=mock_open, read_data='1\r\n2\r\n\r\n3')
     def test_read_input(self, m_open):
         result = read_input('/dummy/filename')
         m_open.assert_called_once_with('/dummy/filename')
-        self.assertListEqual(['1\n2', '3'], result)
+        self.assertListEqual(['1\r\n2', '3'], result)
+
+    def test_parse_lines(self):
+        test_lines = ['1\r\n2', '3']
+        expected_result = [['1', '2'], ['3']]
+        self.assertListEqual(expected_result, parse_lines(test_lines))
