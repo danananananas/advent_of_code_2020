379b44c1788eb8228b5d66424866de7766c93174
dana <danananananas@gmail.com>
2020-12-22 20:17:38 +0100

    add part 2

--- day06/solution.py
+++ day06/solution.py
@@ -1,4 +1,5 @@
-from helper import read_input_w_double_return
+from collections import Counter
+from helper import read_input_w_double_return, read_input_w_double_n_single_return
 
 
 def count_unique_group_answers(group):
@@ -10,9 +11,26 @@ def count_all_unique_group_answers(filename):
     return [count_unique_group_answers(group) for group in group_answers]
 
 
+def count_only_answered_by_all_groups(group):
+    counter = Counter(group[0])
+    for person in group[1:]:
+        counter = counter & Counter(person)
+    return len(counter)
+
+
+def count_all_only_answered_by_all_groups(filename):
+    group_answers = read_input_w_double_n_single_return(filename)
+    return [count_only_answered_by_all_groups(group) for group in group_answers]
+
+
 def solution_1():
     return sum(count_all_unique_group_answers('input.txt'))
 
 
+def solution_2():
+    return sum(count_all_only_answered_by_all_groups('input.txt'))
+
+
 if __name__ == '__main__':
     print(solution_1())
+    print(solution_2())
--- day06/test_solution.py
+++ day06/test_solution.py
@@ -1,27 +1,56 @@
 from unittest import TestCase
 from unittest.mock import patch, mock_open
-from day06.solution import count_unique_group_answers, count_all_unique_group_answers
+from day06.solution import (
+    count_unique_group_answers, count_all_unique_group_answers,
+    count_only_answered_by_all_groups, count_all_only_answered_by_all_groups
+)
 
 
 class TestSolution(TestCase):
-    def test_check_group_answers_ex1(self):
+    def test_count_unique_group_answers_ex1(self):
         test_group = 'abc'
         self.assertEqual(3, count_unique_group_answers(test_group))
 
-    def test_check_group_answers_ex2(self):
+    def test_count_unique_group_answers_ex2(self):
         test_group = 'abac'
         self.assertEqual(3, count_unique_group_answers(test_group))
 
-    def test_check_group_answers_ex3(self):
+    def test_count_unique_group_answers_ex3(self):
         test_group = 'aaaa'
         self.assertEqual(1, count_unique_group_answers(test_group))
 
-    def test_check_group_answers_ex4(self):
+    def test_count_unique_group_answers_ex4(self):
         test_group = 'b'
         self.assertEqual(1, count_unique_group_answers(test_group))
+
+    def test_count_only_answered_by_all_groups_ex1a(self):
+        test_group = ['abc']
+        self.assertEqual(3, count_only_answered_by_all_groups(test_group))
+
+    def test_count_only_answered_by_all_groups_ex1b(self):
+        test_group = ['a', 'b', 'c']
+        self.assertEqual(0, count_only_answered_by_all_groups(test_group))
+
+    def test_count_only_answered_by_all_groups_ex2(self):
+        test_group = ['ab', 'ac']
+        self.assertEqual(1, count_only_answered_by_all_groups(test_group))
+
+    def test_count_only_answered_by_all_groups_ex3(self):
+        test_group = ['a', 'a', 'a', 'a']
+        self.assertEqual(1, count_only_answered_by_all_groups(test_group))
+
+    def test_count_only_answered_by_all_groups_ex4(self):
+        test_group = ['b']
+        self.assertEqual(1, count_only_answered_by_all_groups(test_group))
 
     @patch('builtins.open', new_callable=mock_open, read_data='abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb')
     def test_count_all_unique_group_answers(self, m_open):
         result = count_all_unique_group_answers('/dummy/filename')
         m_open.assert_called_once_with('/dummy/filename')
         self.assertListEqual([3, 3, 3, 1, 1], result)
+
+    @patch('builtins.open', new_callable=mock_open, read_data='abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb')
+    def test_count_all_only_answered_by_all_groups(self, m_open):
+        result = count_all_only_answered_by_all_groups('/dummy/filename')
+        m_open.assert_called_once_with('/dummy/filename')
+        self.assertListEqual([3, 0, 1, 1, 1], result)
