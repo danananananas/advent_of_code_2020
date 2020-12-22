93d95c49e73e6413c8c23963277f6e51fdbd0390
dana <danananananas@gmail.com>
2020-12-22 19:58:52 +0100

    function count_all_unique_group_answers now does all, got solution for part 1

--- day06/solution.py
+++ day06/solution.py
@@ -1,6 +1,18 @@
+from helper import read_input_w_double_return
+
+
 def count_unique_group_answers(group):
     return len(set(group))
 
 
 def count_all_unique_group_answers(filename):
-    pass
+    group_answers = read_input_w_double_return(filename)
+    return [count_unique_group_answers(group) for group in group_answers]
+
+
+def solution_1():
+    return sum(count_all_unique_group_answers('input.txt'))
+
+
+if __name__ == '__main__':
+    print(solution_1())
